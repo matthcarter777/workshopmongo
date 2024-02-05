1986fcb7a89b4ca016b8c50c2480a1454fe339c9
mateus <mateushbsb@gmail.com>
2024-02-05 10:26:23 -0300

    Inserção de usuário com POST

--- src/main/java/com/mateus/silva/workshopmongo/resources/UserResource.java
+++ src/main/java/com/mateus/silva/workshopmongo/resources/UserResource.java
@@ -1,11 +1,13 @@
 package com.mateus.silva.workshopmongo.resources;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mateus.silva.workshopmongo.domain.User;
 import com.mateus.silva.workshopmongo.dto.UserDTO;
 import com.mateus.silva.workshopmongo.services.UserService;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,7 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -38,6 +41,17 @@ public class UserResource {
     User user = service.findById(id);
 
     return ResponseEntity.ok().body(new UserDTO(user));
+  }
+
+  @PostMapping()
+  public ResponseEntity<Void> create(@RequestBody UserDTO objDto) {
+    User user = service.fromDTO(objDto);
+
+    user = service.insert(user);
+
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+    return ResponseEntity.created(uri).build();
   }
 
 }
--- src/main/java/com/mateus/silva/workshopmongo/services/UserService.java
+++ src/main/java/com/mateus/silva/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mateus.silva.workshopmongo.domain.User;
+import com.mateus.silva.workshopmongo.dto.UserDTO;
 import com.mateus.silva.workshopmongo.repository.UserRepository;
 import com.mateus.silva.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,13 @@ public class UserService {
     Optional<User> obj = repo.findById(id);
     return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
   }
+
+  public User insert(User user) {
+    return repo.insert(user);
+  }
+
+  public User fromDTO(UserDTO objDto) {
+    return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+  }
+
 }
