8db2dc712d1b6b8ae23f742458e72a12fae71e03
mateus <mateushbsb@gmail.com>
2024-02-07 16:01:12 -0300

    Consulta simples com query methods

--- src/main/java/com/mateus/silva/workshopmongo/resources/PostResource.java
+++ src/main/java/com/mateus/silva/workshopmongo/resources/PostResource.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.mateus.silva.workshopmongo.domain.Post;
 import com.mateus.silva.workshopmongo.domain.User;
 import com.mateus.silva.workshopmongo.dto.UserDTO;
+import com.mateus.silva.workshopmongo.resources.util.URL;
 import com.mateus.silva.workshopmongo.services.PostService;
 import com.mateus.silva.workshopmongo.services.UserService;
 
@@ -17,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,6 +38,15 @@ public class PostResource {
     Post post = service.findById(id);
 
     return ResponseEntity.ok().body(post);
+  }
+
+  @GetMapping("/titlesearch")
+  public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+    text = URL.decodeParam(text);
+
+    List<Post> posts = service.findByTitle(text);
+
+    return ResponseEntity.ok().body(posts);
   }
 
 }
