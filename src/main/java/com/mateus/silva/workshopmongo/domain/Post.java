a6cf2cac12ec6ea8aabed5deba282cd0b10e3732
mateus <mateushbsb@gmail.com>
2024-02-07 13:47:12 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/mateus/silva/workshopmongo/domain/Post.java
+++ src/main/java/com/mateus/silva/workshopmongo/domain/Post.java
@@ -1,9 +1,12 @@
 package com.mateus.silva.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import com.mateus.silva.workshopmongo.dto.AuthorDTO;
+import com.mateus.silva.workshopmongo.dto.CommentDTO;
 
 public class Post implements Serializable {
   private static final long serialVersionUID = 1L;
@@ -13,6 +16,8 @@ public class Post implements Serializable {
   private String title;
   private String body;
   private AuthorDTO author;
+
+  private List<CommentDTO> comments = new ArrayList<>();
 
   public Post() {
   }
@@ -63,6 +68,14 @@ public class Post implements Serializable {
 
   public void setAuthor(AuthorDTO author) {
     this.author = author;
+  }
+
+  public List<CommentDTO> getComments() {
+    return comments;
+  }
+
+  public void setComments(List<CommentDTO> comments) {
+    this.comments = comments;
   }
 
   @Override
