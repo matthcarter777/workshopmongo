f69f745041dc4bd6631e4e87f4a23540d0a5f09d
mateus <mateushbsb@gmail.com>
2024-02-07 09:45:53 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/mateus/silva/workshopmongo/domain/Post.java
+++ src/main/java/com/mateus/silva/workshopmongo/domain/Post.java
@@ -3,6 +3,8 @@ package com.mateus.silva.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.mateus.silva.workshopmongo.dto.AuthorDTO;
+
 public class Post implements Serializable {
   private static final long serialVersionUID = 1L;
 
@@ -10,12 +12,12 @@ public class Post implements Serializable {
   private Date date;
   private String title;
   private String body;
-  private User author;
+  private AuthorDTO author;
 
   public Post() {
   }
 
-  public Post(String id, Date date, String title, String body, User author) {
+  public Post(String id, Date date, String title, String body, AuthorDTO author) {
     this.id = id;
     this.date = date;
     this.title = title;
@@ -55,11 +57,11 @@ public class Post implements Serializable {
     this.body = body;
   }
 
-  public User getAuthor() {
+  public AuthorDTO getAuthor() {
     return author;
   }
 
-  public void setAuthor(User author) {
+  public void setAuthor(AuthorDTO author) {
     this.author = author;
   }
 
